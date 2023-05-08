f039f8358cc49c37992d1dad27085ffb81f9ab8e
Aysikh <amy.sikhammountry@gmail.com>
2023-05-08 16:12:23 -0400

    Minor tweaks to fix warnings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CountriesPopulationChart from "./CountriesPopulationChart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container mx-auto">
+            <h2 className="text-2xl font-bold text-center my-4">
+                Countries Populations by Continent
+            </h2>
+            <CountriesPopulationChart />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/CountriesPopulationChart.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useRef, useState } from "react";
+import Chart from "chart.js/auto";
+
+function CountiesPopulationChart() {
+    const [chartData, setChartData] = useState(null);
+    const [selectedContinent, setSelectedContinent] = useState("");
+    const [selectedSort, setSelectedSort] = useState("");
+    const [isLinearScale, setIsLinearScale] = useState(true); 
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://restcountries.com/v3.1/all");
+                const data = await response.json();
+                const continentData = {};
+
+                data.forEach((country) => {
+                    const continent = country.continents;
+
+                    if (!continentData[continent]) {
+                        continentData[continent] = [];
+                    }
+
+                    continentData[continent].push({
+                        name: country.name.common,
+                        population: country.population,
+                        flag: country.flag,
+                    });
+                });
+
+                // Sort the data based on selectedSort value
+                Object.values(continentData).forEach((continent) => {
+                    continent.sort((a, b) => {
+                        if (selectedSort === "asc") {
+                            return a.population - b.population;
+                        } else if (selectedSort === "desc") {
+                            return b.population - a.population;
+                        }
+                        return 0;
+                    });
+                });
+
+                setChartData(continentData);
+            } catch (error) {
+                console.error("Error fetching API data:", error);
+            }
+        };
+
+        fetchData();
+    }, [selectedSort]);
+
+    useEffect(() => {
+        if (chartData) {
+            const data = selectedContinent ? chartData[selectedContinent] : [];
+
+            const chartConfig = {
+                type: "bar",
+                data: {
+                    labels: data.map((country) => country.flag + " " + country.name),
+                    datasets: [
+                        {
+                            label: "Population",
+                            data: data.map((country) => country.population),
+                            backgroundColor: "rgba(255, 99, 132, 0.2)",
+                            borderColor: "rgba(255, 99, 132, 1)",
+                            borderWidth: 1,
+                            hoverBackgroundColor: "rgba(255, 99, 132, 0.5)",
+                            hoverBorderColor: "rgba(255, 99, 132, 1)",
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: "Country",
+                            },
+                            ticks: {
+                                maxRotation: 90,
+                                minRotation: -5,
+                            },
+                        },
+                        y: {
+                            type: isLinearScale ? 'linear' : 'logarithmic',
+                            title: {
+                                display: true,
+                                text: "Population",
+                            },
+                        },
+                    },
+                },
+            };
+
+            if (chartRef.current) {
+                chartRef.current.destroy();
+            }
+
+            const chartCanvas = document.getElementById("chart");
+            if (chartCanvas) {
+                chartRef.current = new Chart(chartCanvas, chartConfig);
+            }
+        }
+    }, [chartData, selectedContinent, isLinearScale]);
+
+    const handleSelectedContinentChange = (event) => {
+        setSelectedContinent(event.target.value);
+    };
+
+    const handleSelectedSortChange = (event) => {
+        setSelectedSort(event.target.value);
+    };
+
+    const handleSelectedLogChange = (event) => {
+        setIsLinearScale(event.target.value === 'linear');
+
+        setChartData((prevConfig) => {
+            const newOptions = { ...prevConfig.options };
+            if (newOptions.scales && newOptions.scales.y) {
+              newOptions.scales.y.type = event.target.value;
+            }
+            return { ...prevConfig, options: newOptions };
+          });
+    }
+
+    return (
+        <div className="container mx-auto">
+            {chartData && (
+                <div className="flex justify-center my-4 relative inline-block">
+                    <select className="px-4 py-2 border border-gray-300 rounded-md mx-4"
+                            value={selectedContinent}
+                            onChange={handleSelectedContinentChange}>
+
+                        <option value="">Select Continent</option>
+
+                        {Object.keys(chartData).map((continent) => (
+                            <option key={continent} value={continent}>
+                                {continent}
+                            </option>
+                        ))}
+                    </select>
+
+                    <select className="px-4 py-2 border border-gray-300 rounded-md"
+                            value={selectedSort}
+                            onChange={handleSelectedSortChange}>
+
+                        <option value="">Select Sort</option>
+                        <option value="asc">Sort Ascending</option>
+                        <option value="desc">Sort Descending</option>
+                    </select>
+
+                    <select className="px-4 py-2 border border-gray-300 rounded-md mx-4"
+                            value={isLinearScale ? 'linear' : 'logarithmic'}
+                            onChange={handleSelectedLogChange}>
+                        <option value="linear">Linear Scale</option>
+                        <option value="logarithmic">Logarithmic Scale</option>
+                    </select>
+                </div>
+            )}
+
+            {selectedContinent ? (
+                <div className="flex justify-center my-4">
+                    <canvas id="chart" />
+                </div>
+            ) : (
+                <p className="text-center text-red-700">
+                    Please select a continent to view its countries and their populations.
+                </p>
+            )}
+        </div>
+    );
+}
+
+export default CountiesPopulationChart;
